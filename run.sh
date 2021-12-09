51dca459e1b181f4b6fdc109c65bbf69eddca8f6
Ryan Miller <ryan@devopsmachine.com>
2021-12-09 13:24:20 -0500

    Add service account and SCC to OpenShift

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Building ${BUILDER_IMAGE_NAME} locally..."
 buildah build -t "${BUILDER_IMAGE_NAME}:latest" .
+
+echo "Pushing ${BUILDER_IMAGE_NAME}..."
 skopeo copy "container-storage:${BUILDER_IMAGE_NAME}:latest" \
             "docker://${BUILDER_IMAGE_NAME}:latest"
+
+echo "Adding buildah-sa service account to OpenShift..."
+oc apply -f manifests/serviceaccount.yaml
+
+echo "Adding SCC to buildah-sa..."
+oc adm policy add-scc-to-user anyuid -z buildah-sa
