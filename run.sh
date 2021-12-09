de7a15e721224e57bfa14a2f792783bca060a56c
Ryan Miller <ryan@devopsmachine.com>
2021-12-09 13:19:41 -0500

    Move variable outside of run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-BUILDER_IMAGE_NAME='example'
-
 buildah build -t "${BUILDER_IMAGE_NAME}:latest" .
 skopeo copy "container-storage:${BUILDER_IMAGE_NAME}:latest" \
             "docker://${BUILDER_IMAGE_NAME}:latest"
